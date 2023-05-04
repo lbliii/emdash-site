90de84b643c2bac0321c6e6222ff1101cba040f0
L.B <mrlawrencelane@gmail.com>
2023-05-03 21:01:35 -0400

    offering and showcase

---
+++ src/lib/components/Showcase.svelte
@@ -0,0 +1,62 @@
+<script>
+
+    const showcaseItems = [
+        {
+            title: "title",
+            link: "/",
+            image: "https://images.pexels.com/photos/16128360/pexels-photo-16128360.jpeg",
+            description: "",
+        }, 
+        {
+            title: "title",
+            link: "/",
+            image: "https://images.pexels.com/photos/16128360/pexels-photo-16128360.jpeg",
+            description: "",
+        }, 
+        {
+            title: "title",
+            link: "/",
+            image: "https://images.pexels.com/photos/16128360/pexels-photo-16128360.jpeg",
+            description: "",
+        }, 
+        {
+            title: "title",
+            link: "/",
+            image: "https://images.pexels.com/photos/16128360/pexels-photo-16128360.jpeg",
+            description: "",
+        }, 
+        {
+            title: "title",
+            link: "/",
+            image: "https://images.pexels.com/photos/16128360/pexels-photo-16128360.jpeg",
+            description: "",
+        }, 
+        {
+            title: "title",
+            link: "/",
+            image: "https://images.pexels.com/photos/16128360/pexels-photo-16128360.jpeg",
+            description: "",
+        }, 
+        
+    ]
+
+    let showcaseContainer
+
+    const handleScroll = () => {
+        const scrollTop = showcaseContainer.scrollTop
+        const delta = scrollTop - (showcaseContainer.dataset.prevScrollTop || 0)
+        showcaseContainer.scrollLeft += delta
+        showcaseContainer.dataset.prevScrollTop = scrollTop
+    }
+   
+</script>
+
+<div id="showcase-container" class="container">
+  <div id="showcase" class="flex flex-row overflow-x-auto">
+    {#each showcaseItems as {title, link, image, description}}
+      <div class="flex-shrink-0 min-w-min variant-ghost-surface block w-48 h-64 lg:w-72 lg:h-96 bg-contain rounded-md mx-4" style="background-image: url('{image}')">
+      </div>
+    {/each}
+  </div>
+</div>
+
--- src/lib/components/Sign.svelte
+++ src/lib/components/Sign.svelte
@@ -1,16 +1,15 @@
-
-<div class="h-screen bg-contain bg-no-repeat bg-right" style="background-image: url('computer.png')">
-  <div class="container ">
-    <div class="container pb-11">
-        <div id="Sign" class="text-6xl lg:text-8xl font-bold text-success-500 bungee emdash">
-            Welcome <br>to EMDASH
+<div class="h-screen bg-contain bg-no-repeat bg-right bg-" style="background-image: url('computer.png')">
+  <div id="Sign" class="container flex flex-col justify-center">
+    <div class="container ">
+        <div class="text-6xl lg:text-8xl font-bold text-success-500 bungee emdash text-center max-w-sm mt-2">
+            Design <br> as a <br> Service
         </div> 
   </div>
-    <p class="max-w-lg py-7">
-    Our UX/UI and product design agency takes a modern approach to creating unique and engaging digital experiences for our clients. With expertise in tools such as SvelteKit, Hugo, Supabase, Netlify, and more, we work closely with our clients to understand their needs and goals, and deliver creative solutions that exceed expectations. Our design style draws inspiration from the funky aesthetics of the 90s, resulting in bold and energetic designs that provide exceptional user experiences. Whether you need a complete website redesign, custom digital product, or help with UX/UI design, we're dedicated to delivering high-quality work that helps our clients stand out in their industries.
-    </p>
+    <div class="max-w-md text-2xl">
+    Pay-as-you go design subscriptions <br> for websites & apps.
+    </div>
 
-    <button type="button" class="btn px-24 border-b-4 border-tertiary-700 variant-filled-tertiary  font-black"> CTA </button>
+    <a class="btn max-w-md mt-4 border-b-4 border-tertiary-700 variant-filled-tertiary  font-black" href="#meow"> Compare Plans </a>
   </div>
 </div>
 
